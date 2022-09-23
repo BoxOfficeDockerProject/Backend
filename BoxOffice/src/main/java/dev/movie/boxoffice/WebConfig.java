0c2c2d9b558dfad04542c2ef8b037ec7385e7074
Jupiter-J <namju2912@gmail.com>
2022-09-23 20:39:19 +0900

    feat: CORS 에러 해결

--- BoxOffice/src/main/java/dev/movie/boxoffice/WebConfig.java
+++ BoxOffice/src/main/java/dev/movie/boxoffice/WebConfig.java
@@ -11,17 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://172.19.02:8080", //backend
-                        "http://172.18.0.2:3000", //frontend
-                        "http://localhost:3000",
-                        "http://127.0.0.1:3000",
-                        "http://localhost:8080",
-                        "http://127.0.0.1:8080"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+               .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS)
+               .allowCredentials(false)
+               .maxAge(MAX_AGE_SECS)
         ;
     }
 }
